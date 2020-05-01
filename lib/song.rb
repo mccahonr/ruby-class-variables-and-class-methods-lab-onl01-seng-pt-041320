ce63dce05dc5eb9dba799d338988898d955aae16
Ryan McCahon <mccahonr@yahoo.com>
2020-05-01 18:33:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,21 @@
 class Song
+  
+  @@count = 0
+  @@genres = []
+  
   attr_accessor :name, :artist, :genre
   
-  def initialize(name)
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@count+=1
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
 end
